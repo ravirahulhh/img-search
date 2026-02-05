e1ebc6ad600cf58108ce0019594aa776b12f861c
calehh <caleihhmy@gmail.com>
2026-02-05 19:10:42 +0800

    up

--- build_index.py
+++ build_index.py
@@ -1,4 +1,17 @@
 import argparse
+import os
+
+# Match environment safeguards used in src.cli to avoid OpenMP / BLAS
+# runtime conflicts on macOS (which can cause segfaults when using
+# PyTorch + faiss together).
+if "KMP_DUPLICATE_LIB_OK" not in os.environ:
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+if "OMP_NUM_THREADS" not in os.environ:
+    os.environ["OMP_NUM_THREADS"] = "1"
+if "OPENBLAS_NUM_THREADS" not in os.environ:
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"
+if "MKL_NUM_THREADS" not in os.environ:
+    os.environ["MKL_NUM_THREADS"] = "1"
 
 from src.config import paths, index as index_cfg
 from src.indexer import build_pq_index
